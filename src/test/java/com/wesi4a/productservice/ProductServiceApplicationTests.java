2d15ff9bd59e283c1422935969eb568edecb69a7
Veselin Kokoshkov <veselinak1510@gmail.com>
2023-12-12 19:28:56 +0200

    Test

--- src/test/java/com/wesi4a/productservice/ProductServiceApplicationTests.java
+++ src/test/java/com/wesi4a/productservice/ProductServiceApplicationTests.java
@@ -1,13 +1,64 @@
 package com.wesi4a.productservice;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wesi4a.productservice.dto.ProductRequest;
+import com.wesi4a.productservice.dto.ProductResponse;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.shaded.com.fasterxml.jackson.core.JsonProcessingException;
+
+import java.math.BigDecimal;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@Testcontainers
+@AutoConfigureMockMvc
 class ProductServiceApplicationTests {
 
+	@Container
+ 	static PostgreSQLContainer postgreSQLContainer =
+			new PostgreSQLContainer("postgres:14.3");
+
+	@Autowired
+	private MockMvc mockMvc;
+	@Autowired
+	private ObjectMapper objectMapper;
+	@DynamicPropertySource
+	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
+		dynamicPropertyRegistry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+		dynamicPropertyRegistry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+		dynamicPropertyRegistry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+	}
 	@Test
-	void contextLoads() {
+	void shouldCreateProduct() throws Exception {
+		ProductRequest productRequest = getProductRequest();
+		String productRequestString = objectMapper.writeValueAsString(productRequest);
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/product")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(productRequestString))
+				.andExpect(status().isCreated());
+	}
+
+	private ProductRequest getProductRequest() {
+		return ProductRequest.builder()
+				.name("iPhone 13")
+				.description("iPhone 13")
+				.price(BigDecimal.valueOf(1200))
+				.quantity(1)
+				.build();
 	}
 
 }
+
+
